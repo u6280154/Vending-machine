1ec68304951d1f27d2fb6be75215f025a0bd5f11
u6280154 <91524277+u6280154@users.noreply.github.com>
2023-02-10 14:40:57 +0700

    er

--- Vending/testing/test_machine.py
+++ Vending/testing/test_machine.py
@@ -4,7 +4,7 @@ from app import create_app
 def test_machine():
     app = create_app()
     response = app.test_client().post("/addMachine/", json={
-        "code": "OORT_CLOUD",
-        "address": "Xibalba"
+        "code": "Surtr",
+        "address": "Scandinevia"
     })
     assert response.status_code == 400
