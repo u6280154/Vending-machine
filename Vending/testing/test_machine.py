ba473d2d8d9a010c6b20e83ed01231c5a9b7b84d
u6280154 <91524277+u6280154@users.noreply.github.com>
2023-02-20 19:36:56 +0700

    test

--- Vending/testing/test_machine.py
+++ Vending/testing/test_machine.py
@@ -3,8 +3,7 @@ from Vending.app import create_app
 
 def test_machine():
     app = create_app()
-    response = app.test_client().post("/addMachine/", json={
-        "code": "MICTLAN",
-        "address": "Yayauqxi"
-    })
+    response = app.test_client().post(
+        "/addMachine/", json={"code": "MICTLAN", "address": "Yayauqxi"}
+                                      )
     assert response.status_code == 200
