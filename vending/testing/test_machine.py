5e3ef5f0e695c4bcc03fcf5a11ce478c71489e07
u6280154 <91524277+u6280154@users.noreply.github.com>
2023-02-21 19:58:10 +0700

    Update test_machine.py

--- vending/testing/test_machine.py
+++ vending/testing/test_machine.py
@@ -10,6 +10,6 @@ def test_get_all_machine(client: FlaskClient) -> None:
 def test_add_machine(client: FlaskClient) -> None:
     """High level support for doing this and that."""
     response = client.post(
-        "/addMachine/", json={"code": "MAC1234", "address": "Lake134"}
+        "/addMachine/", json={"code": "MAC12345", "address": "Lake1345"}
     )
     assert response.status_code == 200
